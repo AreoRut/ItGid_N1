05337fd708ab95efcc9d72d781631b963cc4e2a7
AreoRut <valdsieg@gmail.com>
2019-10-25 19:06:57 +0500

    First comment

---
+++ scriptN1.js
@@ -0,0 +1,39 @@
+console.log("Areo Siegfreid");
+console.log("1988-23-03");
+console.log("Hello " + "_" + " World");
+//alert(2019 - 202);
+
+//document.getElementById("out").innerHTML = 'ewqwe';
+document.getElementById("out").innerHTML = "2092";
+document.getElementById("spout").innerHTML = "world";
+document.getElementById("three").innerHTML = "Sertification finish";
+document.querySelector('h2.three').innerHTML += "Trtwo";
+document.querySelector('#one').innerHTML = "qeewert";
+//document.querySelector('p.four').innerHTML = "four enable stansive";
+//document.querySelector('h4.four').innerHTML = "five disable amour stand";
+let z4 = document.querySelector('#qw');
+z4.innerHTML += '+к заголовку';
+let z5 = document.querySelector('#qe');
+z5.innerHTML += '+к параграфу';
+let z6 = document.querySelector('.four');
+z6.innerHTML += 'общая к div';
+
+
+
+let dz = document.querySelector('.dz');
+dz.innerHTML += 'Заголовок';
+let dd = document.querySelector('#dd');
+dd.innerHTML += 'Вычисление суммы';
+let a = 6;
+let b = 3;
+let zum = document.querySelector('#zum');
+zum.innerHTML += a / b;
+
+
+
+let d2 = document.querySelector('.test2');
+d2.innerHTML = 'integer value = ';
+d2.innerHTML += "4 = ";
+let d3 = d2.innerHTML += " New Value"
+console.log("вывод " + " " + d3);
+
